c082a1d1b1be894ef63573eab980a924bbf3ec14
Sweta.Dash <sweta.dash@cognizant.com>
2019-08-22 15:09:07 +0530

    Upload New File

---
+++ src/app/create-reminder/reminder.ts
@@ -0,0 +1,10 @@
+// Reminder property values
+export class Reminder {
+    id: String;
+    reminderName: String;
+    reminderDescription: String;
+    reminderType: String;
+    reminderCreatedBy: String;
+    reminderCreationDate: Date;
+    reminderId: String;
+  }
