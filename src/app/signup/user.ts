1f74871c1bd9a73f9a2210455102b09f1efbb6a2
Sweta.Dash <sweta.dash@cognizant.com>
2019-08-22 15:14:14 +0530

    Upload New File

---
+++ src/app/signup/user.ts
@@ -0,0 +1,12 @@
+// User properties
+export class User {
+    public userId: string;
+    public userPassword: string;
+    public firstName: string;
+    public lastName: string;
+    public userRole: string;
+    public userMobile: string;
+    public isChecked: boolean;
+    public userName: string;
+    public userAddedDate: Date;
+}
