081776781d102db42964eb1cf569f96aa0cf6fd1
Sweta.Dash <sweta.dash@cognizant.com>
2019-09-06 18:33:07 +0530

    Update user.service.ts

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -13,7 +13,7 @@ export class UserService {
   constructor(private httpClient: HttpClient, private authService: AuthenticationService) { }
   // For user registration and it will be save to both Mondo DB and MYSQL DB
   registerUserService(user: User) {
-    return this.httpClient.post<User>('http://localhost:8084/v1/user',
+    return this.httpClient.post<User>('http://localhost:8080/api/v1/user',
       { userId: user.userId, userName: user.firstName, userPassword: user.userPassword, userMobile: user.userMobile }, {
         headers: new HttpHeaders().set('authorization', `Bearer ${this.authService.getBearerToken()}`)
       });
@@ -21,7 +21,7 @@ export class UserService {
   }
   // For user authentication post registration
   registerUserAuthService(user: User) {
-    return this.httpClient.post<User>('http://localhost:8083/api/v1/auth/register',
+    return this.httpClient.post<User>('http://localhost:8089/api/v1/auth/register',
       {
         userId: user.userId, userPassword: user.userPassword,
         firstName: user.firstName, lastName: user.lastName, userRole: user.userRole
